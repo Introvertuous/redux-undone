0eaa40d1b2273260c0ea8da92912ba2f25084899
Chris Ryan <theramin108@gmail.com>
2018-08-27 21:14:58 -0400

    feat(rollup): use babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 
@@ -17,5 +18,12 @@ export default {
       sourcemap: true,
     },
   ],
-  plugins: [resolve(), commonjs()],
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+      plugins: ['external-helpers'],
+    }),
+    resolve(),
+    commonjs(),
+  ],
 };

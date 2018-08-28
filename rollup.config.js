87849086084c485682bc689b511e7251220fecc7
Chris Ryan <theramin108@gmail.com>
2018-08-27 20:29:56 -0400

    feat(setup): initial rollup

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+
+import pkg from './package.json';
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true,
+    },
+  ],
+  plugins: [resolve(), commonjs()],
+};

cfc543a80a2a06d351ee731ae76bc8a5fe583a72
Chris Ryan <theramin108@gmail.com>
2018-09-02 19:09:07 -0400

    ehancement(optimization): remove unnecessary redux-thunk

--- example/src/redux/index.js
+++ example/src/redux/index.js
@@ -2,11 +2,10 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { reducers, transformers } from './modules';
 import undone from '../../../dist';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
 
 export default () => {
   const rootReducer = combineReducers(reducers);
-  const middleware = [thunk, undone.createMiddleware(transformers)];
+  const middleware = [undone.createMiddleware(transformers)];
   const composeEnhancers = composeWithDevTools({});
 
   return createStore(

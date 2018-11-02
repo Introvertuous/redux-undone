2a9cbcb41ed8af49e5a48be3004967f37b15d413
Chris Ryan <theramin108@gmail.com>
2018-11-02 15:52:03 -0400

    feat(perf): scrap deepmerge utility

--- src/utils/merge.ts
+++ src/utils/merge.ts
@@ -1,3 +1,17 @@
-import deepmerge from 'deepmerge';
+const isObject = (value: any) =>
+  typeof value === 'object' && !Array.isArray(value);
 
-export default (src: object, dst: object) => deepmerge({ ...src }, { ...dst });
+const merge = (src: object, dst: object) =>
+  Object.entries(dst).reduce(
+    (acc, [dstProperty, dstValue]) => {
+      const srcValue = src[dstProperty];
+      acc[dstProperty] =
+        isObject(srcValue) && isObject(dstValue)
+          ? merge(srcValue, dstValue)
+          : dstValue;
+      return acc;
+    },
+    { ...src }
+  );
+
+export default merge;

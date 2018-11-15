977286c8c59c69dfbc8a5bd1e4dc95e5ea38ab13
Chris Ryan <theramin108@gmail.com>
2018-11-15 10:57:49 -0500

    feat(api): export types for undo and redo

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-import { redo, undo } from './lib/actions';
+import { redo, REDO, undo, UNDO } from './lib/actions';
 import createMiddleware from './lib/create_middleware';
 
 export default {
+  REDO,
+  UNDO,
   createMiddleware,
   redo,
   undo,
